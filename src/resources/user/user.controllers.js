c4a4c27d353a78e2ad9c9315e8f9c48591f4af30
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-03-03 00:12:32 -0500

    update user controller

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -11,5 +11,23 @@ module.exports = {
       console.error(e);
       res.status(400).end();
     }
+  },
+  updatePerson: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        {
+          new: true
+        }
+      )
+        .lean()
+        .exec();
+
+      res.status(200).json({ data: user });
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
   }
 };
