1e6d5db41cfa399366d2e4316e4702b2e08249be
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-03-02 23:59:01 -0500

    user model is almost ready

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -2,3 +2,89 @@
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: value => {
+      if (!validator.isEmail(value)) {
+        throw new Error({ error: "Invalid Email address" });
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+// hash password with bcrypt
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  });
+});
+
+// add token to user model
+userSchema.methods.generateAuthToken = async function() {
+  // Generate an auth token for the user
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.methods.verifyToken = token =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+      if (err) return reject(err);
+      resolve(payload);
+    });
+  });
+
+// compare password with bcrypt and we would use this in the signin authe
+userSchema.methods.checkPassword = function(password) {
+  const passwordHash = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(same);
+    });
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
