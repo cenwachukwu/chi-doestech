3baac5ea7cb232c174445e6b9c2d0223bd38df82
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-03-03 00:10:18 -0500

    setting up controllers

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -1,1 +1,15 @@
 const User = require("./user.model");
+
+module.exports = {
+  getAllUsers: async (req, res) => {
+    try {
+      const allUsers = await User.find()
+        .lean()
+        .exec();
+      res.status(200).json({ data: allUsers });
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
+  }
+};
